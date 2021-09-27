3431d65b3d82d0edab6ceacc74ef7617d5f6770c
Michelle Zietlow <MZietlow@District158.org>
2021-09-27 17:07:55 -0500

    first commit

---
+++ LearnGit.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class LearnGit here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LearnGit
+{
+    public static void main(String[] args) {
+        System.out.println("Ms. Z was here");
+    }
+}
